8f3af8c40db7f2eb1a1b27041fa60da1696f6ce3
ThayroneVenancio <thayrone@gmail.com>
2019-10-28 20:01:53 -0300

    Interface Usuario

---
+++ ProjetoCRUD/src/univs/edu/usuario/Main.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package univs.edu.usuario;
+
+/**
+ *
+ * @author victo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Usuario usuario = new Usuario();
+
+        UsuarioDAO dao = new UsuarioDAO();
+        
+        usuario.setLogin("admin");
+        usuario.setSenha("admin");
+        
+        dao.salvar(usuario);
+        
+//        usuario.setLogin("admin");
+//        usuario.setSenha("admin");
+//        dao.editar(usuario);
+        
+//        usuario = dao.pesquisar(1);
+//        System.out.println(usuario.getIdUsuario());
+//        System.out.println(usuario.getLogin());
+//        System.out.println(usuario.getSenha());
+    }
+
+}
